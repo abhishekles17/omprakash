04850c7006c9622c28e7141374d065b60117ac30
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Air.local>
2023-01-01 18:06:57 +0530

    small fix

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,26 @@
 import "./main.scss";
 import Header from "./Header";
 import Landing from "./Landing";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import SidePanel from "./common/SidePanel";
 import RoundTable from "./RoundTable";
 
 const Main = () => {
   const [open, setOpen] = useState();
+  const divRef = useRef(null);
+
+
+  useEffect(() => {
+    if(window.screen.width <= 767) {
+      divRef.current.style.height = '100vh';
+    }
+  },[])
   const handleClose = () => {
     setOpen(false);
   };
 
   return (
-    <div className="container">
+    <div ref={divRef} className="container">
       <Header />
       <Landing setOpen={setOpen} />
       <SidePanel open={open} onClose={handleClose}>
